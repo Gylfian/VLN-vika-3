20f41c044da13d162b09b4b495e17763283ffeec
Anitasol <anitaj15@ru.is>
2015-12-11 11:57:56 +0000

    MainWindow

--- VLNvika3/mainwindow.cpp
+++ VLNvika3/mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "listoptions.h"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,4 +12,36 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_pushButtonAddEntry_clicked()
+{
+
+}
+
+void MainWindow::on_pushButtonAddConnection_clicked()
+{
+
+}
+
+void MainWindow::on_pushButtonSearch_clicked()
+{
+
+}
+
+void MainWindow::on_pushButtonDelete_clicked()
+{
+
+}
+
+void MainWindow::on_pushButtonExit_clicked()
+{
+
+}
+
+void MainWindow::on_pushButtonListOptions_clicked()
+{
+    ListOptions listoptions;
+    listoptions.setModal(true);
+    listoptions.exec();
 }
